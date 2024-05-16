170ca7c743e1dae2321c7d30d02bb2a84f6595ef
jwhogg <jhogg2@sheffielc.ac.uk>
2024-05-16 16:24:07 +0100

    updated auth

--- lib/auth/authpage.dart
+++ lib/auth/authpage.dart
@@ -12,26 +12,53 @@ class _AuthPageState extends State<AuthPage> {
   final TextEditingController _passwordController = TextEditingController();
   bool _isLoading = false;
 
-  void _signUp() async {
+  Future<void> _signUp() async {
+    try {
     setState(() {
       _isLoading = true;
     });
     final AuthResponse response = await supabase.auth.signUp(
-      email: _emailController.text,
+      email: _emailController.text.trim(),
       password: _passwordController.text,
     );
     setState(() {
       _isLoading = false;
     });
-    final Session? session = response.session;
-    final User? user = response.user;
-    if (session != null && user != null) {
-      // Navigate to home page
-      Navigator.pushReplacementNamed(context, '/home');
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Sign Up failed! Please try again.')),
-      );
+    //all ok:
+    if (mounted) { //mounted means if widget is on screen
+      // Navigator.pushReplacementNamed(context, '/home'); //dont want to go to home page yet
+      //add to database
+      try {
+      await supabase.from('Users').insert({'email': _emailController.text.trim()});
+      }
+      catch (e) {print('failed to add to db'); print(e);}
+        ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Check your email for a login link!')),);
+        _emailController.clear();
+    }
+    }
+    on AuthException catch (error) { //if error with supabase auth
+      if (mounted) {
+          SnackBar(
+            content: Text(error.message),
+            backgroundColor: Theme.of(context).colorScheme.error,
+        );
+      }
+    }
+    catch (error) {
+      if (mounted) {
+          SnackBar(
+            content: const Text('Unexpected error occurred'),
+            backgroundColor: Theme.of(context).colorScheme.error,
+        );
+      }
+    }
+    finally {
+      if (mounted) {
+        setState(() {
+          _isLoading = false;
+        });
+      }
     }
   }
 
